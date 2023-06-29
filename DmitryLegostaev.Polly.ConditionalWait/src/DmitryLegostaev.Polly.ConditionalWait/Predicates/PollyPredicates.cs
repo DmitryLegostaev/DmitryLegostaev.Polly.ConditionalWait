a5719e2532ffb5bb5805cedef3b99e45485586df
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-06-29 21:32:13 +0300

    Added Predicates

---
+++ DmitryLegostaev.Polly.ConditionalWait/src/DmitryLegostaev.Polly.ConditionalWait/Predicates/PollyPredicates.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomationFramework.Utilities.Polly;
+
+public static class PollyPredicates
+{
+    public static Func<T, bool> IsNotNullPredicate<T>() => t => t is not null;
+    public static readonly Func<bool, bool> IsTruePredicate = t => t;
+}
